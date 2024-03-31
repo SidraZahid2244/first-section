0e1fe52effbe08a2f0f0e9695cd65d3fd2dfe00c
SidraZahid2244 <165553872+SidraZahid2244@users.noreply.github.com>
2024-03-31 02:59:39 -0700

    these are my first 6 files

---
+++ second class/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let message = "sidra";
+let message1 = 20;
+let message2 = true;
+console.log(message, message1, message2);
---
+++ second class/main.ts
@@ -0,0 +1,13 @@
+let message = "sidra"
+let message1 = 20
+let message2 = true
+console.log(message,message1,message2);
+
+
+
+
+
+
+
+
+
